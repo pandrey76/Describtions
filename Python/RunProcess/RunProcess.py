db6890c46386d7db8755dc180a5409ee52bd7fb0
pandrey76 <pandrey76@yandex.ru>
2020-12-14 16:40:18 +0300

    Add example to start process from python with detaching parent process.

--- Python/RunProcess/RunProcess.py
+++ Python/RunProcess/RunProcess.py
@@ -11,9 +11,27 @@ file_path = os.path.join(file_path, "HelloWorld.PS1")
 print(file_path)
 # creation_flags = win32process.DETACHED_PROCESS
 # creation_flags = win32process.CREATE_NEW_CONSOLE
-process = subprocess.Popen(["powershell.exe", file_path], shell=False,
-                           creationflags=win32process.CREATE_NEW_CONSOLE)
-sleep(5)
+
+# Запуск процесса который будет работать после того как
+# породимвший его процесс закончит работу, но при этом создаётся
+# ещё один экземпляр консоли
+##########################################################
+
+# process = subprocess.Popen(["powershell.exe", file_path], shell=False,
+#                            creationflags=win32process.CREATE_NEW_CONSOLE)
+
+##########################################################
+
+
+# RunProcess with DETACHED_PROCESS
+# Всё работает как с флагом CREATE_NEW_CONSOLE, только
+# без создания экземпляра консоли
+##########################################################
+
+file_path = "c:\\Work\\WindowsRemoteManeger\\PS\\Update.ps1"
+subprocess.Popen(["powershell.exe", file_path], close_fds=True)
+sleep(10)
+##########################################################
 
 # уСТАНОВКА OFFLINE-PACKAGES
 # pip.exe wheel --wheel-dir "c:\\Work\\offline_packages" -r "C:\\Work\\acvpServer\\backend\\requirements.txt"
