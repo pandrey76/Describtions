e1d411cb32481fb46dca4f89e2330929b611dfeb
pandrey76 <pandrey76@yandex.ru>
2021-07-09 10:22:58 +0300

    Added tmux.sh file.

---
+++ Linux/tmux/tmux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+tmux new-session -s run-watch-fork \; \
+  send-keys 'echo First pane!' C-m \; \
+   send-keys "echo Activate first pane!" C-m \; \
+   split-window -v -p 10 \; \
+   send-keys 'echo Second pane!' C-m \; \
+   send-keys 'echo Activate second pane!' C-m \; \
+   select-pane -U \; \
+   split-window -h \; \
+   send-keys 'echo Third pane!' C-m \; \
+   send-keys 'echo Activate third pane!' C-m \; \
+   select-pane -D \; \
+   split-window -h \; \
+   send-keys 'echo Forth pane!' C-m \; \
+   send-keys 'echo Activate forth pane!' C-m \; \
+   split-window -h \; \
+   send-keys 'echo Fifth pane!' C-m \; \
+   send-keys 'echo Activate fifth pane!' C-m \; \
+   select-pane -U \; \
+   split-window -h \; \
+   send-keys 'watch -n 5 "find . -type f | grep -P 'DEBUG_CryptographyVerificationServer\.*' | xargs rm"' C-m \; \
+   split-window -v \; \
+   send-keys "watch -n 5 "find . -type f | grep -P 'testSession_([[:digit:]]+).json' | xargs rm"" C-m \; \
