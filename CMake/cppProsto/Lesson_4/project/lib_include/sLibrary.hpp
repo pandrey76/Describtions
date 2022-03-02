47d03a4c88054900443f2e8431acc146a792d4d5
pandrey76 <pandrey76@yandex.ru>
2022-03-02 17:54:24 +0300

    Lesson # 4 of cmake package education by YouTube blogger cppProsto.
    Running project with shared library.

--- CMake/cppProsto/Lesson_4/project/lib_include/sLibrary.hpp
+++ CMake/cppProsto/Lesson_4/project/lib_include/sLibrary.hpp
@@ -2,20 +2,20 @@
 #define _LIBRARY_H_
 
 #if defined(_WIN32) || defined(_WIN64)
-    #define MYLIB_EXPORT __declspec(dllexport)
-    #define MYLIB_IMPORT __declspec(dllimport)
+
+  #define MYLIB_EXPORT __declspec(dllexport)
+  #define MYLIB_IMPORT __declspec(dllimport)
 
 #else
-    #define MYLIB_EXPORT __attribute__((visibility("default")))
-    #define MYLIB_IMPORT __attribute__((visibility("default")))
-    // Возможно с помощью данного макроса можно маркировать функции или классы как не экспортируемые.
-    #define MYLIB_HIDDEN __attribute__((visibility("hidden")))
+  #define MYLIB_EXPORT __attribute__((visibility("default")))
+  #define MYLIB_IMPORT __attribute__((visibility("default")))
+  #define MYLIB_HIDDEN __attribute__((visibility("hidden")))
+#endif
 
-// Через макрос определяем импортируемые функции и классы в динамической библиотеке
 struct MYLIB_IMPORT sLibrary
 {
-    void print_info();
+  void print_info();
 };
 
-#endif /*  _LIBRARY_H_ */
 
+#endif /* _LIBRARY_H_ */
--- CMake/cppProsto/Lesson_4/project/src/stest.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "stest.hpp"
-
-void sTest::print_i()
-{
-    std::cout << "m_i = " << m_i << std::endl;
-}
