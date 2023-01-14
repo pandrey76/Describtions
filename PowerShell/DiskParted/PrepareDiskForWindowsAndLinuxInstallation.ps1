92564fafc23194bf596f6e02bc20ca4859e274b1
pandrey76 <pandrey76@yandex.ru>
2023-01-14 20:52:26 +0300

    Added PrepareDiskForWindowsAndLinuxInstallation.ps1 file.

---
+++ PowerShell/DiskParted/PrepareDiskForWindowsAndLinuxInstallation.ps1
@@ -0,0 +1,10 @@
+﻿# New-Item -Path ".\ScriptForDiskpart_PrepareHDD.txt" -Verbose999
+# Start-Process -FilePath "diskpart.exe"  -ArgumentList "/s c:\\Users\\м\\Documents\\ScriptForDiskpart_ViewListOfDisks.txt > .\\temp.txt" -Verb RunAs
+
+# Start-Process -FilePath "diskpart.exe"  -ArgumentList "/s c:\\Users\\м\\Documents\\ScriptForDiskpart_ViewListOfDisks.txt" -Wait
+# > powershell.exe -executionpolicy bypass -File .\PrepareDiskForWindowsAndLinux.ps1
+
+# ---------------  Delete partition from selected drive ---------------
+
+
+ #>
